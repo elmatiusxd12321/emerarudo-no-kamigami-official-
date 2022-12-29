6882d65401968b9933c52380b9e489e40423dc6c
Gamematweb <118083998+elmatiusxd12321@users.noreply.github.com>
2022-12-29 10:54:14 -0600

    Add files via upload

---
+++ code.js
@@ -0,0 +1,24 @@
+function inicio()
+{
+window.location="index.html"
+}
+function t1()
+{
+window.location="index copy 1.html"
+}
+function guion()
+{
+window.location="index copy 2.html"
+}
+function t2()
+{
+window.location="index copy 3.html"
+}
+function guion2()
+{
+window.location="index copy 4.html"
+}
+function creditos()
+{
+window.location="windowcr.html"
+}
