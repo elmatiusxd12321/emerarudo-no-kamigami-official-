d79828fd429d853540580f406435aa00828ae8ca
Gamematweb <118083998+elmatiusxd12321@users.noreply.github.com>
2022-12-30 17:19:27 -0600

    Add files via upload

--- code.js
+++ code.js
@@ -4,21 +4,54 @@ window.location="index.html"
 }
 function t1()
 {
-window.location="index copy 1.html"
+window.location="temporada 1.html"
 }
 function guion()
 {
-window.location="index copy 2.html"
+window.location="windowguion1.html"
 }
 function t2()
 {
-window.location="index copy 3.html"
+window.location="temporada 2.html"
 }
 function guion2()
 {
-window.location="index copy 4.html"
+window.location="windowguion2.html"
 }
 function creditos()
 {
-window.location="windowcr.html"
+window.location="creditos.html"
+}
+function vpnen()
+{
+window.location="index copy.html"
+}
+///english///
+function start()
+{
+window.location="index copy.html"
+}
+function t1en()
+{
+window.location="temporada 1 copy.html"
+}
+function guionen()
+{
+window.location="windowguion1 copy.html"
+}
+function t2en()
+{
+window.location="temporada 2 copy.html"
+}
+function guion2en()
+{
+window.location="windowguion2 copy.html"
+}
+function credits()
+{
+window.location="creditos copy.html"
+}
+function vpnes()
+{
+window.location="index.html"
 }
